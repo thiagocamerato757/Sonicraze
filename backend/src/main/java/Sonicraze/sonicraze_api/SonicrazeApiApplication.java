c607f85fd3f196a29648424d1a78b408d25871db
thiagocamerato757 <cameratothiago@gmail.com>
2025-03-14 13:22:04 -0300

    adicao de arquivos de config

---
+++ backend/src/main/java/Sonicraze/sonicraze_api/SonicrazeApiApplication.java
@@ -0,0 +1,11 @@
+package Sonicraze.sonicraze_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonicrazeApiApplication {
+    public static void main(String [] args) {
+        SpringApplication.run(SonicrazeApiApplication.class, args);
+    }
+}
